68303c06965b90fffd9fb2a3dd820160004d9ab5
jtmeros <contacto@jmformoso.net>
2015-12-09 14:46:07 +0100

    Implementación de opcioens
    - Ahora acepta 3 opciones
    - Opción -n para solo números
    - Opción -l para solo letras
    - Opción -nl para números y letras

--- wpassgen.py
+++ wpassgen.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8
-# v0.1.0
+# v0.1.1
 # by jtmeros
 
 import sys, os, string, random
@@ -7,9 +7,17 @@ import sys, os, string, random
 caracteres = string.ascii_letters + string.digits + '!@#$%^&*()' # string que incluye letras, números y caracteres especiales
 random.seed = (os.urandom(1024))
 
-
 if len(sys.argv) >= 2:
 	longitud = int(sys.argv[1]) # longitud elegida por el usuario
+
+	if (str(sys.argv[2]) == "-l"):
+		caracteres = string.ascii_letters
+
+	if (str(sys.argv[2]) == "-n"):
+		caracteres = string.digits
+
+	if (str(sys.argv[2]) == "-nl"):
+		caracteres = string.digits + string.ascii_letters
 
 else:
 	longitud = 10 # longitud por defecto
