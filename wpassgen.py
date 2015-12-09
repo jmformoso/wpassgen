03f197bf9148de0a458d6aeb0c20fde756f401fe
jtmeros <contacto@jmformoso.net>
2015-12-09 14:17:47 +0100

    Añadidos comentarios
    - Nuevos comentarios generales

--- wpassgen.py
+++ wpassgen.py
@@ -4,15 +4,15 @@
 
 import sys, os, string, random
 
-caracteres = string.ascii_letters + string.digits + '!@#$%^&*()'
+caracteres = string.ascii_letters + string.digits + '!@#$%^&*()' # string que incluye letras, números y caracteres especiales
 random.seed = (os.urandom(1024))
 
 
 if len(sys.argv) >= 2:
-	longitud = int(sys.argv[1])
+	longitud = int(sys.argv[1]) # longitud elegida por el usuario
 
 else:
-	longitud = 10
+	longitud = 10 # longitud por defecto
 	
 
-print ''.join(random.choice(caracteres) for i in range(longitud))
+print ''.join(random.choice(caracteres) for i in range(longitud)) # muestra la contraseña
