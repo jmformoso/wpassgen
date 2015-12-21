5c774dad292b5b02f108d2940edce0b19cad1142
jtmeros <jtmeros@MacBook-Pro-de-jtmeros.local>
2015-12-22 00:32:26 +0100

    acepta solo opciones

--- wpassgen.py
+++ wpassgen.py
@@ -7,44 +7,49 @@ import sys, os, string, random
 caracteres = string.ascii_letters + string.digits + '!@#$%+&*-' # string que incluye letras, números y caracteres especiales
 random.seed = (os.urandom(1024))
 
+
 if len(sys.argv) >= 2:
+
+	numargv = 1 # primer argv como el opcional
+	
 
 	try: # si no se introduce un número en el segundo argumento
 		longitud = int(sys.argv[1]) # longitud elegida por el usuario
+		if len(sys.argv) >= 3:
+			numargv = 2 # el argv segundo es el opcional
 	except:
 		longitud = 10 # longitud por defecto
+		
+	
+	if (str(sys.argv[numargv]) == "-l"):
+		caracteres = string.ascii_letters # solo letras
 
+	if (str(sys.argv[numargv]) == "-ll"):
+		caracteres = string.ascii_lowercase #solo letras minúsculas
 
-	if len(sys.argv) >= 3:
-		if (str(sys.argv[2]) == "-l"):
-			caracteres = string.ascii_letters # solo letras
+	if (str(sys.argv[numargv]) == "-lL"):
+		caracteres = string.ascii_uppercase #solo letras mayúsculas
 
-		if (str(sys.argv[2]) == "-ll"):
-			caracteres = string.ascii_lowercase #solo letras minúsculas
+	if (str(sys.argv[numargv]) == "-n"):
+		caracteres = string.digits # solo números
 
-		if (str(sys.argv[2]) == "-lL"):
-			caracteres = string.ascii_uppercase #solo letras mayúsculas
+	if (str(sys.argv[numargv]) == "-s"):
+		caracteres = '!@#$%+&*-' # solo símbolos
 
-		if (str(sys.argv[2]) == "-n"):
-			caracteres = string.digits # solo números
+	if (str(sys.argv[numargv]) == "-p"):
+		caracteres = string.punctuation # solo signos puntuación
 
-		if (str(sys.argv[2]) == "-s"):
-			caracteres = '!@#$%+&*-' # solo símbolos
+	if (str(sys.argv[numargv]) == "-h"):
+		caracteres = string.hexdigits # hexadecimal
 
-		if (str(sys.argv[2]) == "-p"):
-			caracteres = string.punctuation # solo signos puntuación
+	if (str(sys.argv[numargv]) == "-nl"):
+		caracteres = string.digits + string.ascii_letters # letras y números
 
-		if (str(sys.argv[2]) == "-h"):
-			caracteres = string.hexdigits # hexadecimal
+	if (str(sys.argv[numargv]) == "-ns"):
+		caracteres = string.digits + '!@#$%+&*-' # números y símbolos
 
-		if (str(sys.argv[2]) == "-nl"):
-			caracteres = string.digits + string.ascii_letters # letras y números
-
-		if (str(sys.argv[2]) == "-ns"):
-			caracteres = string.digits + '!@#$%+&*-' # números y símbolos
-
-		if (str(sys.argv[2]) == "-ls"):
-			caracteres = string.ascii_letters + '!@#$%+&*-' # letras y símbolos
+	if (str(sys.argv[numargv]) == "-ls"):
+		caracteres = string.ascii_letters + '!@#$%+&*-' # letras y símbolos
 
 else:
 	longitud = 10 # longitud por defecto
