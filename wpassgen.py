61e556174e0cfb161a78f7135277a834ab3c0f82
Jose M. Formoso <contacto@jmformoso.net>
2017-08-30 10:08:07 +0200

    Eliminado símbolo '+'

--- wpassgen.py
+++ wpassgen.py
@@ -4,14 +4,14 @@
 
 import sys, os, string, random
 
-caracteres = string.ascii_letters + string.digits + '!@#$%+&*-' # string que incluye letras, números y caracteres especiales
+caracteres = string.ascii_letters + string.digits + '!@#$%&*-' # string que incluye letras, números y caracteres especiales
 random.seed = (os.urandom(1024))
 
 
 if len(sys.argv) >= 2:
 
 	numargv = 1 # primer argv como el opcional
-	
+
 
 	try: # si no se introduce un número en el segundo argumento
 		longitud = int(sys.argv[1]) # longitud elegida por el usuario
@@ -19,8 +19,8 @@ if len(sys.argv) >= 2:
 			numargv = 2 # el argv segundo es el opcional
 	except:
 		longitud = 10 # longitud por defecto
-		
-	
+
+
 	if (str(sys.argv[numargv]) == "-l"):
 		caracteres = string.ascii_letters # solo letras
 
@@ -34,7 +34,7 @@ if len(sys.argv) >= 2:
 		caracteres = string.digits # solo números
 
 	if (str(sys.argv[numargv]) == "-s"):
-		caracteres = '!@#$%+&*-' # solo símbolos
+		caracteres = '!@#$%&*-' # solo símbolos
 
 	if (str(sys.argv[numargv]) == "-p"):
 		caracteres = string.punctuation # solo signos puntuación
@@ -46,13 +46,13 @@ if len(sys.argv) >= 2:
 		caracteres = string.digits + string.ascii_letters # letras y números
 
 	if (str(sys.argv[numargv]) == "-ns"):
-		caracteres = string.digits + '!@#$%+&*-' # números y símbolos
+		caracteres = string.digits + '!@#$%&*-' # números y símbolos
 
 	if (str(sys.argv[numargv]) == "-ls"):
-		caracteres = string.ascii_letters + '!@#$%+&*-' # letras y símbolos
+		caracteres = string.ascii_letters + '!@#$%&*-' # letras y símbolos
 
 else:
 	longitud = 10 # longitud por defecto
-	
 
-print (''.join(random.choice(caracteres) for i in range(longitud))) # muestra la contraseña
+
+print ''.join(random.choice(caracteres) for i in range(longitud)) # muestra la contraseña
